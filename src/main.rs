d1bc9fd93fae90b5126cadc585f569784cba91b9
Abhinavpatel <121427766+Abhinavpatel00@users.noreply.github.com>
2023-09-29 22:42:55 +0530

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io::prelude::*;
+use std::{io, thread};
+
+fn main() -> io::Result<()> {
+    let mut i = trust::Interface::new()?;
+    eprintln!("created interface");
+    let mut listener = i.bind(3000)?;
+    while let Ok(mut stream) = listener.accept() {
+        eprintln!("got connection!");
+        thread::spawn(move || {
+            stream.write(b"hello from rust-tcp!\n").unwrap();
+            stream.shutdown(std::net::Shutdown::Write).unwrap();
+            loop {
+                let mut buf = [0; 512];
+                let n = stream.read(&mut buf[..]).unwrap();
+                eprintln!("read {}b of data", n);
+                if n == 0 {
+                    eprintln!("no more data!");
+                    break;
+                } else {
+                    println!("{}", std::str::from_utf8(&buf[..n]).unwrap());
+                }
+            }
+        });
+    }
+    Ok(())
+}
